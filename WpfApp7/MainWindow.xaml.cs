80137e7488467cf72d42d5e6025341ee8025256a
GabrielaCyran <84381376+GabrielaCyran@users.noreply.github.com>
2021-05-29 12:07:22 +0200

    Add files via upload

---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            decimal a = decimal.Parse(Text2.Text);
+            decimal b = decimal.Parse(Text3.Text);
+
+            decimal c = a + b;
+
+
+            text1.Text = $" Suma to : { a} + {b} =  {c}";
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            decimal a = decimal.Parse(Text2.Text);
+            decimal b = decimal.Parse(Text3.Text);
+
+            decimal c = a - b;
+
+
+            text1.Text = $" Różnica  to : { a} - {b} =  {c}";
+
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            decimal a = decimal.Parse(Text2.Text);
+            decimal b = decimal.Parse(Text3.Text);
+
+            decimal c = a * b;
+
+
+            text1.Text = $" Iloczyn  to : { a} * {b} =  {c}";
+
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(Text2.Text);
+            double b = double.Parse(Text3.Text);
+
+            double c = a / b;
+
+
+         //  text1.Text = $" Iloraz to  to : { a} / {b} =  {c}";
+
+            if (  a==0 ||b==0)
+            { 
+                MessageBox.Show("Nie dziel przez zero! ");
+                text1.Text = "Nie dziel przez zero!";
+
+            }
+         else
+            {
+                text1.Text = $" Iloraz to  to : { a} / {b} =  {c}";
+            }
+            
+
+            }
+
+        } 
+}
---
+++ WpfApp9/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void but2_Click(object sender, RoutedEventArgs e)
+        {
+
+            //string dodaj = Tekst.Text;
+            if (Tekst.Text != "piwo" && Tekst.Text !="Piwo" && Tekst.Text != "wino" && Tekst.Text != "Wino")
+
+            {
+                Lab1.Content = "Produkty  :" + (List.Items.Add(Tekst.Text) + 1);
+            }
+            if (Czek2.IsChecked == true && Tekst.Text == "piwo" )
+            {
+                MessageBox.Show("Nie jestes pelnoletni!!!");
+                List.Items.Remove("piwo");
+         
+            }
+            if(Czek2.IsChecked == true && Tekst.Text == "Piwo")
+            {
+                MessageBox.Show("Nie jestes pelnoletni!!!");
+                List.Items.Remove("Piwo");
+            }
+            if (Czek2.IsChecked == true && Tekst.Text == "Wino")
+            {
+                MessageBox.Show("Nie jestes pelnoletni!!!");
+                List.Items.Remove("Wino");
+            }
+            if (Czek2.IsChecked == true && Tekst.Text == "wino")
+            {
+                MessageBox.Show("Nie jestes pelnoletni!!!");
+                List.Items.Remove("wino");
+            }
+
+          
+            
+
+                //Lab1.Content = "Produkty  :" + (List.Items.Add(Tekst.Text)+1);
+
+                
+           
+            
+        }
+
+        private void List_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+                
+        }
+
+        private void ricz_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        private void Czek2_Checked(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        private void czek_Checked(object sender, RoutedEventArgs e)
+        {
+
+            
+        }
+
+        private void But_Click(object sender, RoutedEventArgs e)
+        {
+
+            List.Items.Remove(Tekst.Text);
+              Lab1.Content="Produkty :" +  List.Items.Add(Tekst.Text);
+
+            List.Items.Remove(Tekst.Text);
+        }
+
+        private void List_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void Tekst_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Tekst_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //MessageBox.Show("Wpisz produkty do zamówienia ");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+               
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            
+            
+
+
+
+               
+
+
+            
+         
+
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+           
+         
+            
+                Tekst1.Background = Brushes.Green;
+            MessageBox.Show("Opakowanie zielone");
+
+        }
+
+        private void Tekst_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void czerwonyRed_Checked(object sender, RoutedEventArgs e)
+        {
+            Tekst1.Background = Brushes.Red;
+            MessageBox.Show("Opakowanie czerwone");
+        
+
+        }
+    }
+}
